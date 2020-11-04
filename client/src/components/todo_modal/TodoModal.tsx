8f042813378436da27dd1625f7f1ff2c2b888f06
Leonidas51 <kirpichik61@gmail.com>
2020-11-04 18:26:14 +0300

    remove comment, relocate styles

--- client/src/components/todo_modal/TodoModal.tsx
+++ client/src/components/todo_modal/TodoModal.tsx
@@ -3,8 +3,6 @@ import './TodoModal.css';
 import React from 'react';
 
 export default function TodoModal(): JSX.Element {
-  // call props.hide() to close modal
-
   return (
     <React.Fragment>
       <p>Add task modal placeholder</p>
